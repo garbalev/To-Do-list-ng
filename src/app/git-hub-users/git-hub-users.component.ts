91b8aa48988ff2f985f7434f611ab775588be988
Alexander Garbalev <garbalevalex@mail.ru>
2022-03-20 12:48:48 +0300

    gh-usersWithoutStyles

--- src/app/git-hub-users/git-hub-users.component.ts
+++ src/app/git-hub-users/git-hub-users.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, map, Subject, tap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Subject,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { GhUsersService } from '../shared/gh-users.service';
 
 @Component({
@@ -8,28 +15,26 @@ import { GhUsersService } from '../shared/gh-users.service';
   styleUrls: ['./git-hub-users.component.scss'],
 })
 export class GitHubUsersComponent implements OnInit {
-  private keyUpSubj = new Subject<any>();
+  public keyUpSubj$ = new Subject<any>();
 
   users$: any;
+
+  users: any;
 
   constructor(public gitHubUsersService: GhUsersService) {}
 
   ngOnInit(): void {
-    this.keyUpSubj
-      .pipe(debounceTime(750), distinctUntilChanged())
-      .subscribe((value) => {
-        console.log(value);
-        this.users$ = this.gitHubUsersService.getUsers(value).pipe(
-          map((el) => el.items),
-          tap((el) => {
-            console.log(el);
-          })
-        );
-      });
+    this.users$ = this.keyUpSubj$.pipe(
+      debounceTime(750),
+      distinctUntilChanged(),
+      switchMap((val) => this.gitHubUsersService.getUsers(val)),
+      map((response) => response.items),
+      tap((el) => console.log(el))
+    );
   }
 
   getInfo(event: any) {
     const value = event.target.value;
-    this.keyUpSubj.next(value);
+    this.keyUpSubj$.next(value);
   }
 }
